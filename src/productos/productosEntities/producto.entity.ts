d2e4bb423bedcf1bc09c6dd75675ae1011d4eb6f
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-19 17:51:08 -0400

    feat: adicion de entidad productos

---
+++ src/productos/productosEntities/producto.entity.ts
@@ -0,0 +1,17 @@
+import { Auditoria } from 'src/comun/entities/auditoria.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class Producto extends Auditoria {
+  @Column({ length: 150 })
+  nombre: string;
+
+  @Column({ type: 'text', nullable: true })
+  descripcion: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  precio: number;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  disponibilidad: string;
+}
