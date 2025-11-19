8f81c651ac20d671d3f68e235329ddcfeb0cfc20
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-19 19:13:09 -0400

    feat: importar servicios de producto y categoria

--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -4,10 +4,19 @@ import { ProductosController } from './productoControllers/productos.controller'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './productosEntities/producto.entity';
 import { CategoriaProducto } from './productosEntities/categoriaProducto.entity';
+import { ProductoRepository } from './productoRepositories/producto.repository';
+import { categoriaProductoRepository } from './productoRepositories/categoriaProducto.repository';
+import { CategoriaProductoService } from './productoService/categoriaProducto.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Producto, CategoriaProducto])],
   controllers: [ProductosController],
-  providers: [ProductosService],
+  providers: [
+    ProductosService,
+    CategoriaProductoService,
+    ProductoRepository,
+    categoriaProductoRepository,
+  ],
+  exports: [ProductoRepository, categoriaProductoRepository],
 })
 export class ProductosModule {}
