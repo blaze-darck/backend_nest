37399b409b2078dcb49be19f5cc3c3054dd299db
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-18 20:19:52 -0400

    refactor: actualizar repositorios importando desde carpeta repositories

--- src/usuarios/services/rol.service.ts
+++ src/usuarios/services/rol.service.ts
@@ -2,40 +2,40 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Rol } from '../entities/rol.entity';
+import { RolRepository } from '../repositories/rol.repository';
 
 @Injectable()
 export class RolService {
-  constructor(
-    @InjectRepository(Rol)
-    private readonly rolRepo: Repository<Rol>,
-  ) {}
+  constructor(private readonly rolRepository: RolRepository) {}
 
   // Obtener solo roles activos
   findAll() {
-    return this.rolRepo.find({ where: { activo: true } });
+    return this.rolRepository.find({ where: { activo: true } });
   }
 
   async findOne(id: number) {
-    const rol = await this.rolRepo.findOne({ where: { id, activo: true } });
+    const rol = await this.rolRepository.findOne({
+      where: { id, activo: true },
+    });
     if (!rol) throw new NotFoundException('Rol no encontrado');
     return rol;
   }
 
   create(data: any) {
-    const rol = this.rolRepo.create(data);
-    return this.rolRepo.save(rol);
+    const rol = this.rolRepository.create(data);
+    return this.rolRepository.save(rol);
   }
 
   async update(id: number, data: any) {
     const rol = await this.findOne(id);
     Object.assign(rol, data);
-    return this.rolRepo.save(rol);
+    return this.rolRepository.save(rol);
   }
 
   // Borrado lógico
   async desactivar(id: number) {
     const rol = await this.findOne(id);
     rol.activo = false;
-    return this.rolRepo.save(rol);
+    return this.rolRepository.save(rol);
   }
 }
