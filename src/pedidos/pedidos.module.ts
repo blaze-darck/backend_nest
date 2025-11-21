7a083ad812d72deae49964bebcf629222f3f44fb
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-21 00:57:08 -0400

    feat: actualizar imports de pedidos modulo

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PedidosService } from './pedidosServices/pedidos.service';
 import { PedidosController } from './pedidosControllers/pedidos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pedido } from './pedidosEntities/pedidos.entity';
+import { DetallePedido } from './pedidosEntities/detallePedido.entity';
+import { Pago } from './pedidosEntities/pagos.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pedido, DetallePedido, Pago])],
   controllers: [PedidosController],
   providers: [PedidosService],
 })
