47ee812a58c6cb9b18bf70e56a8f8279c8077c32
blaze-darck <paredesgabriel784@gmail.com>
2025-11-23 11:43:07 -0400

    feat:Adicion de metodo de verificacion con gmail

--- src/usuarios/controllers/usuario.controller.ts
+++ src/usuarios/controllers/usuario.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Put, Patch, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Body,
+  Param,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsuarioService } from '../services/usuario.service';
 import { CreateUsuarioDto } from '../dto/usuarios/crearUsuario.dto';
 import { ActualizarUsuarioDto } from '../dto/usuarios/actualizarUsuario.dto';
@@ -6,6 +16,47 @@ import { ActualizarUsuarioDto } from '../dto/usuarios/actualizarUsuario.dto';
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Get('verify/:token')
+  async verifyEmail(@Param('token') token: string) {
+    const usuario = await this.usuarioService.findByToken(token);
+
+    if (!usuario) {
+      throw new NotFoundException('Token inválido');
+    }
+
+    if (
+      usuario.verificacionExpirada &&
+      usuario.verificacionExpirada < new Date()
+    ) {
+      throw new BadRequestException('Token expirado');
+    }
+
+    await this.usuarioService.verifyUsuario(usuario);
+
+    return { message: 'Correo verificado correctamente' };
+  }
+  @Post('verificar-codigo')
+  async verificarCodigo(@Body() body: { correo: string; codigo: string }) {
+    const usuario = await this.usuarioService.findByCorreo(body.correo);
+
+    if (!usuario) throw new NotFoundException('Usuario no encontrado');
+
+    if (usuario.verificacionToken !== body.codigo) {
+      throw new BadRequestException('Código incorrecto');
+    }
+
+    if (
+      usuario.verificacionExpirada &&
+      usuario.verificacionExpirada < new Date()
+    ) {
+      throw new BadRequestException('Código expirado');
+    }
+
+    await this.usuarioService.verifyUsuario(usuario);
+
+    return { message: 'Cuenta verificada correctamente' };
+  }
 
   @Get()
   findAll() {
