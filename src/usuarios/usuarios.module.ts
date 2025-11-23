1f87c511ef89c420bb9b7f084aa4a738e0756e98
blaze-darck <paredesgabriel784@gmail.com>
2025-11-23 11:47:36 -0400

    feat:Integracion del servicio y controlador de verificacion de email para usuarios

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { Usuario } from './entities/usuario.entity';
 import { Rol } from './entities/rol.entity';
@@ -8,16 +9,21 @@ import { RolUsuario } from './entities/rolUsuario.entity';
 import { UsuarioService } from './services/usuario.service';
 import { RolService } from './services/rol.service';
 import { RolUsuarioService } from './services/rolUsuario.service';
+import { EmailService } from './services/email.service';
 
 import { UsuarioController } from './controllers/usuario.controller';
 import { RolController } from './controllers/rol.controller';
 import { RolUsuarioController } from './controllers/rolUsuario.controller';
+
 import { UsuarioRepository } from './repositories/usuario.repository';
 import { RolRepository } from './repositories/rol.repository';
 import { RolUsuarioRepository } from './repositories/rolUsuario.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario, Rol, RolUsuario])],
+  imports: [
+    TypeOrmModule.forFeature([Usuario, Rol, RolUsuario]),
+    ConfigModule, // <-- necesario para inyectar ConfigService
+  ],
   controllers: [UsuarioController, RolController, RolUsuarioController],
   providers: [
     UsuarioService,
@@ -26,7 +32,8 @@ import { RolUsuarioRepository } from './repositories/rolUsuario.repository';
     UsuarioRepository,
     RolRepository,
     RolUsuarioRepository,
+    EmailService,
   ],
-  exports: [UsuarioService, RolService, RolUsuarioService],
+  exports: [UsuarioService, RolService, RolUsuarioService, EmailService],
 })
 export class UsuariosModule {}
